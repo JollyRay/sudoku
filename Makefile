5ddddc2ffddc72273b33560cfe6dd43941bbdc4f
jollyray <jollyray@gmail.com>
2024-10-22 16:10:29 +0300

    feat: start as daphne

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 all:
 	python manage.py runserver 5678
 
+run:
+	daphne sudoku.asgi:application
+
 init: install
 	python manage.py makemigrations
 	python manage.py migrate
--- sudoku/asgi.py
+++ sudoku/asgi.py
@@ -6,8 +6,11 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
-
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sudoku.settings')
+
+import django
+django.setup()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -16,7 +19,6 @@ from django.core.asgi import get_asgi_application
 
 import game.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sudoku.settings')
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
