4aa7e26a8657353ba2d1378589c585f8fd071d3f
jollyray <jollyray@gmail.com>
2024-11-21 01:45:39 +0300

    feat: add cachs to static file name

--- sudoku/settings.py
+++ sudoku/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIR = []
 
-SASS_PROCESSOR_ROOT = STATIC_ROOT
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
