889f50a93f75627f7bd153b31deca71922b769c7
jollyray <jollyray@gmail.com>
2025-04-11 20:25:47 +0300

    feat: general display of elements in lobby has been changed

--- voice/static/voice/js/pageModife.js
+++ voice/static/voice/js/pageModife.js
@@ -1,3 +1,5 @@
+document.getElementById('local-screen-video').addEventListener('click', toggleFullscreen);
+
 function createNewVideoElement(nick, isScreen = false, homeElemnt = undefined){
     homeElemnt = homeElemnt || document.getElementById('video-home');
 
@@ -6,7 +8,11 @@ function createNewVideoElement(nick, isScreen = false, homeElemnt = undefined){
     videoElement.setAttribute('playsinline', '');
     videoElement.setAttribute('autoplay', true);
     videoElement.setAttribute('nick', nick);
-    videoElement.setAttribute('is-screen', isScreen)
+    videoElement.setAttribute('is-screen', isScreen);
+
+    if (isScreen){
+        videoElement.addEventListener('click', toggleFullscreen);
+    }
 
     homeElemnt.appendChild(videoElement);
 
@@ -16,6 +22,8 @@ function createNewVideoElement(nick, isScreen = false, homeElemnt = undefined){
 function toggleScreenVideo(screenElement){
     screenElement = screenElement || document.getElementById('local-screen-video');
     screenElement.classList.toggle('hidden');
+
+    rerender(screenElement);
 }
 
 function isExist(nick) {
@@ -31,5 +39,32 @@ function removeVideo(nick, isScreen = undefined){
         document.querySelectorAll(`video[nick="${nick}"][is-screen=${isScreen}]`).forEach(elem => {
             elem.remove()
         });
+    }
+}
+
+function rerender(element){
+    let tempDisplay = element.style.display;
+    element.style.display = tempDisplay == 'none' ? '' : 'none';
+    element.style.display = tempDisplay;
+}
+
+function toggleFullscreen(event) {
+    let videoElement = event.target;
+    if (!document.fullscreenElement) {
+        if (videoElement.requestFullscreen) {
+            videoElement.requestFullscreen();
+        } else if (videoElement.webkitRequestFullscreen) { /* Safari */
+            videoElement.webkitRequestFullscreen();
+        } else if (videoElement.msRequestFullscreen) { /* IE11 */
+            videoElement.msRequestFullscreen();
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) { /* Safari */
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) { /* IE11 */
+            document.msExitFullscreen();
+        }
     }
 }
